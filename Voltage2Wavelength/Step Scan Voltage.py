3e54db829a2cd750509e1aeb522f03d196c53d49
kiiichi <kichi3744@outlook.com>
2023-05-03 14:11:08 +0800

    fix & upgrade
    fix: 将函数名按命名规范修改
    upgrade: 完成vol到wl的csv转换

--- Voltage2Wavelength/Step Scan Voltage.py
+++ Voltage2Wavelength/Step Scan Voltage.py
@@ -2,7 +2,7 @@ from time import sleep
 from toptica.lasersdk.client import Client, NetworkConnection
 from toptica.lasersdk.client import UserLevel, Subscription, Timestamp, SubscriptionValue
 
-def TopticaSleep(ExtraTime=0):
+def toptica_sleep(ExtraTime=0):
     """Automatically set toptica laser sleep time for tuning voltage or wavelength.
 
     The variables MUST be defined: SetVol, SetWl, LastSetVol, LastSetWl
@@ -10,36 +10,36 @@ def TopticaSleep(ExtraTime=0):
     sleep(1) # Waiting for reaction, can be modified by adding a Callback
     sleep(max(0.02*abs(SetVol-LastSetVol),0.2*abs(SetWl-LastSetWl))) 
     sleep(ExtraTime)
-def ReadVolSet():
+def read_vol_set():
     """Simplify the operation for reading 'laser1:dl:pc:voltage-set'.
 
     No output
     """
     return client.get('laser1:dl:pc:voltage-set', float)
-def ReadVolAct():
+def read_vol_act():
     """Simplify the operation for reading 'laser1:dl:pc:voltage-act'.
 
     No output
     """
     return client.get('laser1:dl:pc:voltage-act',float)
-def ReadWlSet():
+def read_wl_set():
     """Simplify the operation for reading 'laser1:ctl:wavelength-set'.
 
     No output
     """
     return client.get('laser1:ctl:wavelength-set', float)
-def ReadWlAct():
+def read_wl_act():
     """Simplify the operation for reading 'laser1:ctl:wavelength-act'.
 
     No output
     """
     return client.get('laser1:ctl:wavelength-act', float)
-def WriteVol(SetVol=80.):
+def write_vol(SetVol=80.):
     """Simplify the operation for writing 'laser1:dl:pc:voltage-set'
     
     Default Voltage is 80"""
     client.set('laser1:dl:pc:voltage-set', SetVol)
-def WriteWl(SetWl=1550.):
+def write_wl(SetWl=1550.):
     """Simplify the operation for writing 'laser1:ctl:wavelength-set'
     
     Default  Wavelength is 1550"""
@@ -54,8 +54,8 @@ with Client(NetworkConnection(DLCPRO_CONNECTION)) as client:
     # client.set('system-label', 'the 1st laser')
     print("This is a {} with serial number {}.\nLabel is '{}'".format(
         client.get('system-type'), client.get('serial-number'), client.get('system-label', str)))
-    LastSetVol=ReadVolSet()
-    LastSetWl=ReadWlSet()
+    LastSetVol=read_vol_set()
+    LastSetWl=read_wl_set()
 
     """ Setting Scan Parameter """
     StartVol=0.000
@@ -65,15 +65,15 @@ with Client(NetworkConnection(DLCPRO_CONNECTION)) as client:
 
     """ prepare to scan """
     SetVol=StartVol
-    WriteVol(SetVol)
-    WriteWl(SetWl)
-    TopticaSleep()
+    write_vol(SetVol)
+    write_wl(SetWl)
+    toptica_sleep()
     
     """ start scan """
     input("Press any button to start...")
     client.set('laser1:dl:pc:external-input:enabled',False)
     while SetVol <= StopVol:
-        WriteVol(SetVol)
-        LastSetVol=ReadVolSet()
-        TopticaSleep(0.5)
+        write_vol(SetVol)
+        LastSetVol=read_vol_set()
+        toptica_sleep(0.5)
         SetVol = SetVol + ScanStepVol
---
+++ Voltage2Wavelength/Transform Voltage To Wavelength.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+def interpolate_voltage_to_wavelength(voltage, reference_table):
+    """Interpolate the wavelength value for a given voltage value using the reference table. Can be modify by interpolate module in SciPy. Details in 'https://zhuanlan.zhihu.com/p/136700122'
+    
+    Need a reference table and a voltage, return interpolated_wavelength
+    """
+
+    closest_voltage_below = max(filter(lambda v: v <= voltage, reference_table.keys()), default=None)
+    closest_voltage_above = min(filter(lambda v: v >= voltage, reference_table.keys()), default=None)
+
+    # If the two closest voltage values are the same, return the corresponding wavelength value without interpolation
+    if closest_voltage_below == closest_voltage_above:
+        return reference_table[closest_voltage_below]
+    # If the input voltage is outside the range of the reference table, return None
+    if closest_voltage_below is None or closest_voltage_above is None: 
+        return None
+    
+    closest_wavelength_below = reference_table[closest_voltage_below]
+    closest_wavelength_above = reference_table[closest_voltage_above]
+    fraction = (voltage - closest_voltage_below) / (closest_voltage_above - closest_voltage_below)
+    interpolated_wavelength = closest_wavelength_below + fraction * (closest_wavelength_above - closest_wavelength_below)
+    return interpolated_wavelength
+
+
+""" Genarate Reference Dictionary """
+# Import the Excel file as a pandas DataFrame
+reference_data = pd.read_csv('reference_data.csv')
+
+# Transform the DataFrame into a dictionary where the keys are the voltage values and the values are the wavelength values
+reference_table = dict(zip(reference_data['voltage'], reference_data['wavelength']))
+
+
+""" Example usage """
+voltage_values1 = [1.25, 2.8, 142]
+for voltage in voltage_values1:
+    wavelength = interpolate_voltage_to_wavelength(voltage, reference_table)
+    print(f"Input voltage: {voltage}, Output wavelength: {wavelength}")
+
+
+""" Input, Transform, and Output """
+df = pd.read_csv('transforming.csv')
+voltage_values = df['voltage'].tolist() # Convert the voltage values to a list
+# Interpolate the corresponding wavelength values
+wavelength_values = [interpolate_voltage_to_wavelength(v, reference_table) for v in voltage_values]
+
+# Create a new DataFrame object with the voltage and wavelength values
+result_df = pd.DataFrame({'voltage': voltage_values, 'wavelength': wavelength_values})
+
+# Write the results to a new CSV file
+result_df.to_csv('transformed.csv', index=False)
+print('transforming finished')
