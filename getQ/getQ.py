a1e96a71cd194bccc56b93cde796e93f87541ef4
kiiichi <kichi3744@outlook.com>
2023-08-31 11:58:02 +0800

    ...

--- getQ/getQ.py
+++ getQ/getQ.py
@@ -9,13 +9,6 @@ from rigol_osc import osc_ask_data
 import matplotlib.pyplot as plt
 import warnings
 
-# import pandas as pd
-# import tkinter as tk
-# from tkinter import filedialog
-# import matplotlib.pyplot as plt
-# import warnings
-# import re
-
 def interpolate_voltage_to_wavelength_diff(voltage, fiting_curve_name):
     """Modified from looking up reference table and interpolating to get a polynomial fitting from reference table.
     
@@ -23,15 +16,17 @@ def interpolate_voltage_to_wavelength_diff(voltage, fiting_curve_name):
     """
     match fiting_curve_name:
         case 'toptica1':
-            interpolated_wavelength = 1.41735740e-12 * voltage**5 - 5.70767417e-10 * voltage**4 + 1.15683891e-07 * voltage**3 - 1.37073757e-05 * voltage**2 - 1.58771892e-03 * voltage + 3.84508009e-01
+            interpolated_wavelength_diff = 1.41735740e-12 * voltage**5 - 5.70767417e-10 * voltage**4 + 1.15683891e-07 * voltage**3 - 1.37073757e-05 * voltage**2 - 1.58771892e-03 * voltage + 3.84508009e-01
         case 'toptica2':
-            interpolated_wavelength = 1.45757887e-12 * voltage**5 - 7.47082543e-10 * voltage**4 + 1.66903897e-07 * voltage**3 - 1.84056335e-05 * voltage**2 - 1.72441280e-03 * voltage + 4.32064375e-02
+            interpolated_wavelength_diff = 1.45757887e-12 * voltage**5 - 7.47082543e-10 * voltage**4 + 1.66903897e-07 * voltage**3 - 1.84056335e-05 * voltage**2 - 1.72441280e-03 * voltage + 4.32064375e-02
         case _:
             print('[Function]interpolate_voltage_to_wavelength: No such fitting curve name')
 
-    return interpolated_wavelength
+    return interpolated_wavelength_diff
 
-DLCPRO_CONNECTION = '192.168.1.28'
+ip_toptica1 = '192.168.1.28'
+ip_toptica2 = '192.168.1.29'
+DLCPRO_CONNECTION = ip_toptica1
 
 """Get Parameters"""
 with Client(NetworkConnection(DLCPRO_CONNECTION)) as client:
@@ -55,7 +50,6 @@ slope, intercept = np.polyfit(time_values, data[1], 1)
 voltage_fit = slope * time_values + intercept
 voltage_actual_values = voltage_fit * arc_factor + set_voltage
 wavelength_values = interpolate_voltage_to_wavelength_diff(voltage_actual_values, 'toptica1') + act_wavelength
-# wavelength_values = interpolate_voltage_to_wavelength_diff(voltage_actual_values, 'toptica1')
 
 power = data[2]
 
@@ -84,8 +78,6 @@ half_y_x_point = x[half_y_index]
 w = np.abs(half_y_x_point - xc)
 
 popt, pcov = curve_fit(Lorentz, x, y, p0=[y0,A,xc,w], maxfev=100000)
-# popt, pcov = curve_fit(Lorentz, x, y,  maxfev=10000)
-# Evaluate the fitted function over a range of x values
 x_fit = np.linspace(x.min(), x.max(), 100)
 y_fit = Lorentz(x_fit, *popt)
 
@@ -100,14 +92,6 @@ print(f"y0 = {y0:.8f} +/- {np.sqrt(var_y0):.8f}")
 print(f"A = {A:.8f} +/- {np.sqrt(var_A):.8f}")
 print(f"xc = {xc:.8f} +/- {np.sqrt(var_xc):.8f}")
 print(f"w = {w:.8f} +/- {np.sqrt(var_w):.8f}")
-
-# Plot the original data and the fitted function
-plt.plot(x, y, ',', label='Data')
-plt.plot(x_fit, y_fit, label='Fitted Function')
-# plt.xlim(x.min(), x.max())
-plt.legend()
-plt.show()
-
 
 """ Calculate Quality Factor"""
 # extract the wavelength at the corresponding time
@@ -131,3 +115,10 @@ print(f"Q_loaded is {q_loaded:.2e}")
 print(f"Under couple Q_instinct is {q_instinct_under:.2e}")
 print(f"Critical couple Q_instinct is {q_instinct_critical:.2e}")
 print(f"Over couple Q_instinct is {q_instinct_over:.2e}")
+
+# Plot the original data and the fitted function
+plt.plot(x, y, ',', label='Data')
+plt.plot(x_fit, y_fit, label='Fitted Function')
+# plt.xlim(x.min(), x.max())
+plt.legend()
+plt.show()
