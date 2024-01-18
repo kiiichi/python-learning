2550bfce3a685f52ad3d2441b7d29b94a1d75212
kiiichi <kichi3744@outlook.com>
2024-01-18 22:08:01 +0800

    ...

--- allin1/parameter_table.py
+++ allin1/parameter_table.py
@@ -2,15 +2,15 @@ FSR = 72.83 # GHz
 PSG_freq = [0, 36.4368, 36.42754, 36.42427, 36.42261, 36.42153, 36.42084, 36.42041, 36.42005, 36.41966]
 PSG_power = [0, -7.18, -5.08, -6.4, -4.88, -2.98, -2.78, -2.78, -2.78, -2.78]
 
-# HOSTNAME_RP1 = '192.168.1.17' # pumplocker
-# HOSTNAME_RP2 = '192.168.1.3' # locallocker
-# HOSTNAME_RP3 = '192.168.1.27' # MClocker fast loop
-# HOSTNAME_RP4 = '192.168.1.32' # MClocker slow loop 
+HOSTNAME_RP1 = '192.168.1.17' # pumplocker
+HOSTNAME_RP2 = '192.168.1.3' # locallocker
+HOSTNAME_RP3 = '192.168.1.27' # MClocker fast loop
+HOSTNAME_RP4 = '192.168.1.32' # MClocker slow loop 
 
-HOSTNAME_RP1 = '_FAKE_' # pumplocker
-HOSTNAME_RP2 = '_FAKE_' # locallocker
-HOSTNAME_RP3 = '_FAKE_' # MClocker fast loop
-HOSTNAME_RP4 = '_FAKE_' # MClocker slow loop
+# HOSTNAME_RP1 = '_FAKE_' # pumplocker
+# HOSTNAME_RP2 = '_FAKE_' # locallocker
+# HOSTNAME_RP3 = '_FAKE_' # MClocker fast loop
+# HOSTNAME_RP4 = '_FAKE_' # MClocker slow loop
 
 HOSTNAME_PSG = '192.168.1.8'
 HOSTNAME_WS  = '192.168.1.38'
