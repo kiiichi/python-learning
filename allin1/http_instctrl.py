13bdca7fc9369d1f1c1bffbed9cc26ffc04a9086
kiiichi <kichi3744@outlook.com>
2024-01-23 17:41:24 +0800

    add center filter

--- allin1/http_instctrl.py
+++ allin1/http_instctrl.py
@@ -4,7 +4,7 @@ import numpy as np
 from WSMethods import *
 from parameter_table import HOSTNAME_WS
 
-def ws_dualband_setup(center_pump_wl, space_freq, bandwidth_freq, attenuation=[0,0], phase=[0,0], port=[1,1], ip = HOSTNAME_WS):
+def ws_dualband_setup(center_pump_wl, space_freq, bandwidth_freq, attenuation=[0,0,0], phase=[0,0,0], port=[1,1,1], ip = HOSTNAME_WS):
     """ This function is used to generate the .wsp file for dual-band wavesheper filter. Return the center wavelngth of each band on OSA.
 
     Args:
@@ -29,16 +29,24 @@ def ws_dualband_setup(center_pump_wl, space_freq, bandwidth_freq, attenuation=[0
     freq1_h = center_freq_1 + bandwidth_freq/2000
     freq2_l = center_freq_2 - bandwidth_freq/2000
     freq2_h = center_freq_2 + bandwidth_freq/2000
+    freqp_l = center_pump_freq - bandwidth_freq/2000
+    freqp_h = center_pump_freq + bandwidth_freq/2000
 
     wsFreq1 = np.linspace(freq1_l, freq1_h, int((freq1_h-freq1_l)/0.001 + 1)).round(3)
     wsAttn1 = np.zeros(wsFreq1.shape) + attenuation[0]
     wsPhase1 = np.zeros(wsFreq1.shape) + phase[0]
     wsPort1 = np.zeros(wsFreq1.shape) + port[0]
 
+    wsFreqp = np.linspace(freqp_l, freqp_h, int((freqp_h-freqp_l)/0.001 + 1)).round(3)
+    wsAttnp = np.zeros(wsFreqp.shape) + attenuation[1]
+    wsPhasep = np.zeros(wsFreqp.shape) + phase[1]
+    wsPortp = np.zeros(wsFreqp.shape) + port[1]
+
     wsFreq2 = np.linspace(freq2_l, freq2_h, int((freq2_h-freq2_l)/0.001 + 1)).round(3)
-    wsAttn2 = np.zeros(wsFreq2.shape) + attenuation[1]
-    wsPhase2 = np.zeros(wsFreq2.shape) + phase[1]
-    wsPort2 = np.zeros(wsFreq2.shape) + port[1]
+    wsAttn2 = np.zeros(wsFreq2.shape) + attenuation[2]
+    wsPhase2 = np.zeros(wsFreq2.shape) + phase[2]
+    wsPort2 = np.zeros(wsFreq2.shape) + port[2]
+
 
     # Make blank to combin a .wsp file
     # result = requests.get('http://' + ip + '/waveshaper/devinfo').json()
@@ -51,20 +59,25 @@ def ws_dualband_setup(center_pump_wl, space_freq, bandwidth_freq, attenuation=[0
     wsPhaseBlank0 = np.zeros(wsFreqBlank0.shape)
     wsPortBlank0 = np.zeros(wsFreqBlank0.shape)
 
-    wsFreqBlank1 = np.linspace(freq1_h, freq2_l, int((freq2_l-freq1_h)/0.001 + 1)).round(3)
+    wsFreqBlank1 = np.linspace(freq1_h, freqp_l, int((freq2_l-freq1_h)/0.001 + 1)).round(3)
     wsAttnBlank1 = np.zeros(wsFreqBlank1.shape)
     wsPhaseBlank1 = np.zeros(wsFreqBlank1.shape)
     wsPortBlank1 = np.zeros(wsFreqBlank1.shape)
 
-    wsFreqBlank2 = np.linspace(freq2_h, e, int((e-freq2_h)/0.001 + 1)).round(3)
+    wsFreqBlank2 = np.linspace(freqp_h, freq2_l, int((e-freq2_h)/0.001 + 1)).round(3)
     wsAttnBlank2 = np.zeros(wsFreqBlank2.shape)
     wsPhaseBlank2 = np.zeros(wsFreqBlank2.shape)
     wsPortBlank2 = np.zeros(wsFreqBlank2.shape)
 
-    wsFreq = np.hstack ([wsFreqBlank0, wsFreq1, wsFreqBlank1, wsFreq2, wsFreqBlank2])
-    wsAttn = np.hstack ([wsAttnBlank0, wsAttn1, wsAttnBlank1, wsAttn2, wsAttnBlank2])
-    wsPhase = np.hstack ([wsPhaseBlank0, wsPhase1, wsPhaseBlank1, wsPhase2, wsPhaseBlank2])
-    wsPort = np.hstack ([wsPortBlank0, wsPort1, wsPortBlank1, wsPort2, wsPortBlank2])
+    wsFreqBlank3 = np.linspace(freq2_h, e, int((e-freq2_h)/0.001 + 1)).round(3)
+    wsAttnBlank3 = np.zeros(wsFreqBlank3.shape)
+    wsPhaseBlank3 = np.zeros(wsFreqBlank3.shape)
+    wsPortBlank3 = np.zeros(wsFreqBlank3.shape)
+
+    wsFreq = np.hstack ([wsFreqBlank0, wsFreq1, wsFreqBlank1, wsFreqp, wsFreqBlank2, wsFreq2, wsFreqBlank3])
+    wsAttn = np.hstack ([wsAttnBlank0, wsAttn1, wsAttnBlank1, wsAttnp, wsAttnBlank2, wsAttn2, wsAttnBlank3])
+    wsPhase = np.hstack ([wsPhaseBlank0, wsPhase1, wsPhaseBlank1, wsPhasep, wsPhaseBlank2, wsPhase2, wsPhaseBlank3])
+    wsPort = np.hstack ([wsPortBlank0, wsPort1, wsPortBlank1, wsPortp, wsPortBlank2, wsPort2, wsPortBlank3])
 
     r = uploadProfile(ip, wsFreq, wsAttn, wsPhase, wsPort)
     print(f'Dual band filter set on center: {center_pump_wl}')
