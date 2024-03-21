cd939290200f40939d30716de4ec1149072ae775
kiiichi <kichi3744@outlook.com>
2024-03-21 10:56:26 +0800

    Update parameter_table.py

--- allin1/parameter_table.py
+++ allin1/parameter_table.py
@@ -1,8 +1,8 @@
 FSR = 72.83 # GHz
 PSG_freq = [0, 36.4368, 36.42754, 36.42427, 36.42261, 36.42153, 36.42084, 36.42041, 36.42005, 36.41966]
 PSG_power = [0, -7.18, -5.08, -6.4, -4.88, -2.98, -2.78, -2.78, -2.78, -2.78]
-toptica1_wl_bias = 0
-toptica2_wl_bias = 0
+toptica1_wl_bias = -0.166
+toptica2_wl_bias = 0.205
 
 HOSTNAME_RP1 = '192.168.1.17' # pumplocker
 HOSTNAME_RP2 = '192.168.1.3' # locallocker
