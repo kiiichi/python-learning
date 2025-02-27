5164a747542bd2868315d6139c3937e765f26efd
kiiichi <kichi3744@outlook.com>
2025-02-27 21:31:22 +0800

    ..

--- NKT Photonics/SDK/Examples/DLL_Example_Python/Extreme_Emission_On.py
+++ NKT Photonics/SDK/Examples/DLL_Example_Python/Extreme_Emission_On.py
@@ -1,6 +1,6 @@
 from NKTP_DLL import *
 
-result = registerWriteU8('COM27', 15, 0x30, 0x03, -1)
+result = registerWrite('COM5', 15, 0x30, 0x03, -1)
 print('Setting emission ON - Extreme:', RegisterResultTypes(result))
 
 
