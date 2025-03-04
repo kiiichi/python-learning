b4b71721bd8e8b0b776a12e8babbd50bce53e798
kiiichi <kichi3744@outlook.com>
2025-03-04 16:41:58 +0800

    ...

---
+++ allin1/RigolOSCTest.py
@@ -0,0 +1,13 @@
+import pyvisa as visa
+
+rm = visa.ResourceManager()
+# print(rm.list_resources())
+oscilloscope = rm.open_resource('TCPIP0::192.168.1.9::inst0::INSTR')
+# print(oscilloscope.query('*IDN?'))  # Query the identification string of the oscilloscope
+# oscilloscope.write(':MEASure:ITEM VAVG,CHAN1')
+print(oscilloscope.query(':MEASure:ITEM? VAVG,CHAN1'))
+print(oscilloscope.query(':MEASure:ITEM? VAVG,CHAN1'))
+oscilloscope.close()
+# oscilloscope.open()
+print(oscilloscope.query(':MEASure:ITEM? VAVG,CHAN1'))
+oscilloscope.close()
--- allin1/parameter_table.py
+++ allin1/parameter_table.py
@@ -7,18 +7,20 @@ PSG_power = [-7.18, -7.18, -5.08, -6.4, -4.88, -2.98, -2.78, -2.78, -2.78, -2.78
 toptica1_wl_bias = -0.166
 toptica2_wl_bias = 0.205
 
-HOSTNAME_RP1 = '192.168.1.17' # pumplocker
-HOSTNAME_RP2 = '192.168.1.3' # locallocker
-HOSTNAME_RP3 = '192.168.1.27' # MClocker fast loop
-HOSTNAME_RP4 = '192.168.1.32' # MClocker slow loop 
+# HOSTNAME_RP1 = '192.168.1.17' # pumplocker
+# HOSTNAME_RP2 = '192.168.1.3' # locallocker
+# HOSTNAME_RP3 = '192.168.1.27' # MClocker fast loop
+# HOSTNAME_RP4 = '192.168.1.32' # MClocker slow loop 
 
-# HOSTNAME_RP1 = '_FAKE_' # pumplocker
-# HOSTNAME_RP2 = '_FAKE_' # locallocker
-# HOSTNAME_RP3 = '_FAKE_' # MClocker fast loop
-# HOSTNAME_RP4 = '_FAKE_' # MClocker slow loop
+HOSTNAME_RP1 = '_FAKE_' # pumplocker
+HOSTNAME_RP2 = '_FAKE_' # locallocker
+HOSTNAME_RP3 = '_FAKE_' # MClocker fast loop
+HOSTNAME_RP4 = '_FAKE_' # MClocker slow loop
 
 HOSTNAME_PSG = '192.168.1.8'
 HOSTNAME_WS  = '192.168.1.38'
 
 HOSTNAME_DLCPRO1 = '192.168.1.28'
 HOSTNAME_DLCPRO2 = '192.168.1.29'
+
+HOSTNAME_RIGOLOSC = 'TCPIP0::192.168.1.9::inst0::INSTR'
