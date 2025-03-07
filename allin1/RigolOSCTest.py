9a2babaf978fa39fffc5a0bc11386a8a1142e32e
kiiichi <kichi3744@outlook.com>
2025-03-07 15:43:58 +0800

    ..

--- allin1/RigolOSCTest.py
+++ allin1/RigolOSCTest.py
@@ -1,8 +1,9 @@
 import pyvisa as visa
+from parameter_table import HOSTNAME_RIGOLOSC
 
 rm = visa.ResourceManager()
-# print(rm.list_resources())
-oscilloscope = rm.open_resource('TCPIP0::192.168.1.9::inst0::INSTR')
+print(rm.list_resources())
+oscilloscope = rm.open_resource(HOSTNAME_RIGOLOSC)
 # print(oscilloscope.query('*IDN?'))  # Query the identification string of the oscilloscope
 # oscilloscope.write(':MEASure:ITEM VAVG,CHAN1')
 print(oscilloscope.query(':MEASure:ITEM? VAVG,CHAN1'))
