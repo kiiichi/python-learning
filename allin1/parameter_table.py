194683ec156b97065f0051370b09d13205b90d30
kiiichi <kichi3744@outlook.com>
2025-03-12 10:54:27 +0800

    Update parameter_table.py

--- allin1/parameter_table.py
+++ allin1/parameter_table.py
@@ -12,15 +12,15 @@ GUI_RP2 = False
 GUI_RP3 = False
 GUI_RP4 = False
 
-# HOSTNAME_RP1 = '192.168.1.17' # pumplocker
-# HOSTNAME_RP2 = '192.168.1.3' # locallocker
-# HOSTNAME_RP3 = '192.168.1.27' # MClocker fast loop
-# HOSTNAME_RP4 = '192.168.1.32' # MClocker slow loop 
+HOSTNAME_RP1 = '192.168.1.17' # pumplocker
+HOSTNAME_RP2 = '192.168.1.3' # locallocker
+HOSTNAME_RP3 = '192.168.1.27' # MClocker fast loop
+HOSTNAME_RP4 = '192.168.1.32' # MClocker slow loop 
 
-HOSTNAME_RP1 = '_FAKE_' # pumplocker
-HOSTNAME_RP2 = '_FAKE_' # locallocker
-HOSTNAME_RP3 = '_FAKE_' # MClocker fast loop
-HOSTNAME_RP4 = '_FAKE_' # MClocker slow loop
+# HOSTNAME_RP1 = '_FAKE_' # pumplocker
+# HOSTNAME_RP2 = '_FAKE_' # locallocker
+# HOSTNAME_RP3 = '_FAKE_' # MClocker fast loop
+# HOSTNAME_RP4 = '_FAKE_' # MClocker slow loop
 
 HOSTNAME_PSG = 'TCPIP0::192.168.1.8::inst0::INSTR'
 HOSTNAME_WS  = '192.168.1.38'
